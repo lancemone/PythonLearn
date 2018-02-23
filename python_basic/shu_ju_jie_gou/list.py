384283abe28962638730663f16b0f0f41618777d
lance mone <motao0011@gmail.com>
2018-02-23 21:31:48 +0800

    test

--- python_basic/shu_ju_jie_gou/list.py
+++ python_basic/shu_ju_jie_gou/list.py
@@ -67,3 +67,6 @@ lst01.sort()  # 参数为空时默认为升序排列
 lst01.sort(reverse=True)  # 通过设置参数reverse=True，转换为降序排列
 # 升降序排列也可以使用函数sorted(iterable,cmp，key，reverse)，参数iterable为可迭代对象；参数cmp为函数，参数key为函数，reverse为布尔值
 # sorted()函数不会改变原列表
+print(sorted(lst01))  # 输出显示升序列表
+print(sorted(lst01, reverse=True))  # 输出显示降序列表
+print(lst01)  # 输出显示原列表
--- simple_project/test.py
+++ simple_project/test.py
@@ -1,5 +1,11 @@
-list1 = []  # 创建空的列表
-list2 = [1]
-list3 = [1, 2, 3, 4]
-classmates = ['莫涛', '明明', '方刚']
-print(list1, list2, list3, classmates)
+lst01 = [3, 1, 5, 7, 6, 9]
+lst01.reverse()  # 反向排序：使用reverse()函数
+print(lst01)
+# 升降排序：使用sort(cmp,key，reverse)函数，参数cmp为函数，参数key为函数，reverse为布尔值（True和False）
+lst01.sort()  # 参数为空时默认为升序排列
+lst01.sort(reverse=True)  # 通过设置参数reverse=True，转换为降序排列
+# 升降序排列也可以使用函数sorted(iterable,cmp，key，reverse)，参数iterable为可迭代对象；参数cmp为函数，参数key为函数，reverse为布尔值
+# sorted()函数不会改变原列表
+print(sorted(lst01))  # 输出显示升序列表
+print(sorted(lst01, reverse=True))  # 输出显示降序列表
+print(lst01)  # 输出显示原列表
