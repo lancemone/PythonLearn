e289a0dce7f6525f8ff1ee080787a8aa3f818fc2
lancemone <motao0011@gmail.com>
2018-03-10 17:53:40 +0800

    new

--- python_basic/functions/recursion.py
+++ python_basic/functions/recursion.py
@@ -32,3 +32,48 @@ def factorial1(n):
 
 
 print(factorial1(5))
+
+
+# 幂运算
+def power(x, y):  # 定义函数计算参数的x的y次幂
+
+    if y == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(y):
+            result *= x
+        return result
+
+
+print(power(2, 4))
+
+
+# 使用递归函数幂运算
+def power_recusion(x, y):
+    if y == 0:
+        return 1
+    elif y == 1:
+        return x
+    else:
+        return x * power_recusion(x, y - 1)
+
+
+print(power_recusion(2, 5))
+
+
+# 二分查询
+def search(seq, number, lower, upper):  # 定义函数,参数seq为要查询的序列,参数number为要查询的数字
+    # 参数lower为查找区间的下限值,参数upper为查找区间的上限值
+    if lower == upper:  # 查找区间仅剩一个数字时，即找到查询结果，停止循环调用
+        assert number != seq[upper], '这里有问题！'  # 如果不相等会抛出异常AssertionError（断言错误）,可以尝试把"=="改为"!="
+        print(str(lower))  # 显示输出结果为：66
+    else:  # 查找区间有多个数字时，继续查找
+        middle = (lower + upper) // 2  # 通过整除计算，获取查找区间数字的中间值
+        if number > seq[middle]:  # 判断查找的数字是否大于中间值
+            return search(seq, number, middle + 1, upper)  # 如果大于中间值，中间值作为查找区间下限值，继续查找
+        else:
+            return search(seq, number, lower, middle)  # 如果小于中间值，中间值作为查找区间上限值，继续查找
+
+
+search(range(0, 100), 66, 0, 100)  # 显示输出结果为：66
